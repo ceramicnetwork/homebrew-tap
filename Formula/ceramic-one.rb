9b96128de6940e1d646d961f9ef1ca5d3fac5063
Joel Thorstensson <joel@3box.io>
2024-07-01 09:35:45 +0100

    Remove trailing whitespace

--- Formula/ceramic-one.rb
+++ Formula/ceramic-one.rb
@@ -10,7 +10,7 @@ class CeramicOne < Formula
     url "https://github.com/ceramicnetwork/rust-ceramic/releases/download/v0.25.0/ceramic-one_aarch64-apple-darwin_brew-test.tar.gz"
     sha256 "5470e699784ec7a679937ad4a0c5f19c7559b025b680b0c7206c78c7e4e58df5"
   end
-  
+
   def install
     bin.install "ceramic-one"
   end
